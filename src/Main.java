e0391dcfaa2abd4df5ac9b69036ac142c34d658c
yassine ELANIQ <yassaniq@gmail.com>
2025-04-01 16:29:45 +0000

    feat : ajout de la fonction puissance

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,12 @@ import java.util.Scanner;
 
 public class Main {
 
+    static float a,b;
+
     public static Scanner scanner=new Scanner(System.in);
 
     public static void Addition(){
-        float a,b;
+
         System.out.print("entrer a : ");
         a=scanner.nextFloat();
         System.out.print("entrer b : ");
@@ -18,7 +20,7 @@ public class Main {
     }
 
     public static void Soutraction(){
-        float a,b;
+
         System.out.print("entrer a : ");
         a=scanner.nextFloat();
         System.out.print("entrer b : ");
@@ -27,7 +29,7 @@ public class Main {
     }
 
     public static void Multiplication(){
-        float a,b;
+
         System.out.print("entrer a : ");
         a=scanner.nextFloat();
         System.out.print("entrer b : ");
@@ -36,24 +38,33 @@ public class Main {
     }
 
     public static void Division(){
-        float a,b;
-        System.out.print("entrer a : ");
+
+        System.out.print("entrer divident : ");
         a=scanner.nextFloat();
-        System.out.print("entrer b : ");
+        System.out.print("entrer diviseur : ");
         b=scanner.nextFloat();
         if(b==0){
             System.out.println("la division sur 0 est impossible dans R");
             do {
-                System.out.println("entrer nouveau b : ");
+                System.out.println("entrer nouveau diviseur : ");
                 b=scanner.nextFloat();
             }while(b==0);
-            System.out.println("le resultat de a / b = "+(a/b));
-
-
+            System.out.println("le resultat  = "+(a/b));
         }else {
-            System.out.println("le resultat de a / b = "+(a/b));
-
+            System.out.println("le resultat  = "+(a/b));
         }
+    }
+
+    public static void Puissance(){
+        float resultat = 1;
+        System.out.println("entrer base : ");
+        a= scanner.nextFloat();
+        System.out.println("entrer exposant : ");
+        b= scanner.nextFloat();
+        for(int i=0; i<b;i++){
+            resultat=resultat*a;
+        }
+        System.out.println(a+" a la puissance "+ b +" = "+resultat );
     }
 
     public static void main(String[] args) {
@@ -81,6 +92,7 @@ public class Main {
                     Division();
                     break;
                 case 5 :
+                    Puissance();
                     break;
                 case 6 :
                     break ;
