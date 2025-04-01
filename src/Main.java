d2c310b4b1e494ae35561d5dfd72ae112e954875
yassine ELANIQ <yassaniq@gmail.com>
2025-04-01 15:42:04 +0000

    feat : ajout de la fonction soustraction avec amelioration du menu

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,15 @@ public class Main {
         System.out.println(" la somme de a + b = " +(a+b) );
     }
 
+    public static void Soutraction(){
+        float a,b;
+        System.out.print("entrer a : ");
+        a=scanner.nextFloat();
+        System.out.print("entrer b : ");
+        b=scanner.nextFloat();
+        System.out.println(" la resultat de a - b = " +(a-b) );
+    }
+
     public static void main(String[] args) {
 
         int choix;
@@ -24,7 +33,7 @@ public class Main {
         System.out.println("Hello and welcome!");
 
         do{
-            System.out.println("------MENU-------\n 1-ADDITION : \n 2-QUITTER");
+            System.out.println("------MENU-------\n 1-ADDITION : \n 2-SOUSTRACTION : \n 3-MULTIPLICATION : \n 4-DIVISION : \n 5-PUISSANCE : \n 6-RACINE CARREE : \n 7-FACTORIELLE : \n 8-QUITTER");
             System.out.print("-----ENTRER VOTRE CHOIX : ");
             choix=scanner.nextInt();
 
@@ -33,10 +42,15 @@ public class Main {
                     Addition();
                     break;
                 case 2 :
+                    Soutraction();
+                    break;
+                case 8 :
                     System.out.println("AU REVOIR ET MERCI ");
                     break;
+                default:
+                    System.out.println("CHOIX INTROUVABLE ,RESSAYER !!");
             }
-        }while (choix!=2);
+        }while (choix!=8);
 
     }
 }
