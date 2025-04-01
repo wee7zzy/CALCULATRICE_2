e1de8954a441a5491c3e4c4a4d5ec5c9281fb3d5
yassine ELANIQ <yassaniq@gmail.com>
2025-04-01 15:33:08 +0000

    feat : Ajout de la fonction Addition avec une boucle do while et switch pour le menu

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,42 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+
 
 public class Main {
+
+    public static Scanner scanner=new Scanner(System.in);
+
+    public static void Addition(){
+        float a,b;
+        System.out.print("entrer a : ");
+        a=scanner.nextFloat();
+        System.out.print("entrer b : ");
+        b=scanner.nextFloat();
+        System.out.println(" la somme de a + b = " +(a+b) );
+    }
+
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
 
+        int choix;
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        System.out.println("Hello and welcome!");
+
+        do{
+            System.out.println("------MENU-------\n 1-ADDITION : \n 2-QUITTER");
+            System.out.print("-----ENTRER VOTRE CHOIX : ");
+            choix=scanner.nextInt();
+
+            switch(choix){
+                case 1 :
+                    Addition();
+                    break;
+                case 2 :
+                    System.out.println("AU REVOIR ET MERCI ");
+                    break;
+            }
+        }while (choix!=2);
+
     }
 }
